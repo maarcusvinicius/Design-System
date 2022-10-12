d236c7d07e0dc736347a9661861777182605bd6d
maarcusvinicius <marcus.editor77@gmail.com>
2022-10-12 14:26:24 -0300

    create project

---
+++ form-ds/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css'
+
+export function App() {
+  return (
+    <div>
+      <h1 className='font-bold text-2xl text-gray-100'>Hello margun</h1>
+      
+      <button className='bg-purple-700 font-medium px-4 py-2 rounded text-white hover:bg-purple-500'>Enviar </button>
+    </div>
+  )
+}
---
+++ form-ds/tailwind.config.cjs
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+    colors: {
+      black: '#000',
+      white: '#FFF',
+      transparent: 'transparent',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7C7C8A',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
+
+      purple: {
+        700: '#8811DD',
+        500: '#9036D0',
+      },
+      
+      green: {
+        700: '#038E00',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
+    },
+  },
+  plugins: [],
+}
