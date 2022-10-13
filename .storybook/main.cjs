7f0080453a8d2a8bc3188120273c7d4fd15a3d42
maarcusvinicius <marcus.editor77@gmail.com>
2022-10-13 16:22:11 -0300

    adjusted frontend in npm run

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11"
   ],
   "framework": "@storybook/react",
   "core": {
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,22 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
-import { ReactNode } from 'react';
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className , ...props }: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
 
   return (
     <Comp
       className={clsx(
-        'py-4 px-3 bg-purple-700 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-purple-500',
-
+        'py-3 px-4 bg-purple-700 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-purple-500',
+        className,
       )}
+      {...props}
     >
       {children}
     </Comp>
